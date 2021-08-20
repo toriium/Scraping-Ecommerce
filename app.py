0f9497a9c6ccfefa17a8a2a47979112972f22763
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2021-08-20 17:53:49 -0300

    add info to json return

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, jsonify
 from crawler import laptops_crawler
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/laptops')
 def laptops_list():
-    result = jsonify(laptops_crawler())
-    return result,200
+    result_list_laptops = laptops_crawler()
+
+    actual_datetime = datetime.now()
+    full_datetime = actual_datetime.strftime('%d/%m/%Y %H:%M')
+
+    crawler_return = {
+        'url_site': 'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops',
+        'DateTime': full_datetime,
+        'laptops': result_list_laptops
+    }
+    return jsonify(crawler_return), 200
 
 
 if __name__ == '__main__':
